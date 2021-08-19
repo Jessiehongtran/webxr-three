b8fd0e41425ba5903543896847d5db942a7e1c89
Hong Tran <jessietran@fb.com>
2021-08-19 09:54:00 -0700

    added a new route for getting both question and content

--- server/api/question/question.route.js
+++ server/api/question/question.route.js
@@ -51,6 +51,40 @@ route.get('/:questionId/answers', async (req, res) => {
     const { questionId } = req.params
     try {
         const content = await questionModel.getAnsByQues(questionId)
+        if (content.length > 0){
+            let curSameQues = null
+            let curIndGroup 
+            let i = 0
+            while (content[i]){
+            if (curSameQues === null || content[i].question_text !== curSameQues){
+                //turn answer into array
+                content[i].answers = [
+                    {
+                        answer_text: content[i].answer_text,
+                        next_questionID: content[i].next_questionID
+                    }
+                ]
+                delete content[i].id
+                delete content[i].answer_text
+                delete content[i].next_questionID
+                curIndGroup = i
+                curSameQues = content[i].question_text
+                i += 1
+            }
+            else if (curSameQues && content[i].question_text === curSameQues){
+                //add answer to previous
+                content[curIndGroup].answers.push({
+                answer_text: content[i].answer_text,
+                next_questionID: content[i].next_questionID
+                })
+                //turn the current object to empty
+                content.splice(i, 1)
+            
+            }
+
+            
+            }
+        }
         res.status(200).json(content)
     } catch (err){
         res.status(500).json(err.message)

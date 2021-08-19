4d17acd65a0e80ea20e268880575a748c8813d63
Hong Tran <jessietran@fb.com>
2021-08-19 12:20:44 -0700

    have plus buttons at the end

--- client/index.js
+++ client/index.js
@@ -1,26 +1,26 @@
 const API_URL = ''
 
-const content = [
-    {
-        video_question_id: 1,
-        video: "This is a video",
-        question: "This is a question",
-        answers: [
-            {
-                answer_id: 1,
-                answer_text: "This is answer 1"
-            },
-            {
-                answer_id: 2,
-                answer_text: "This is answer 2"
-            },
-            {
-                answer_id: 3,
-                answer_text: "This is answer 3"
-            }
-        ]
-    }
-]
+// const content = [
+//     {
+//         video_question_id: 1,
+//         video: "This is a video",
+//         question: "This is a question",
+//         answers: [
+//             {
+//                 answer_id: 1,
+//                 answer_text: "This is answer 1"
+//             },
+//             {
+//                 answer_id: 2,
+//                 answer_text: "This is answer 2"
+//             },
+//             {
+//                 answer_id: 3,
+//                 answer_text: "This is answer 3"
+//             }
+//         ]
+//     }
+// ]
 
 let questions
 let answers
@@ -60,7 +60,8 @@ setTimeout(function(){
         }
         setTimeout(function(){
             console.log(ques_ans)
-        }, 5000)
+            fillTable(ques_ans)
+        }, 2000)
     }
 }, 2000)
 
@@ -68,17 +69,39 @@ setTimeout(function(){
 
 const table = document.getElementsByClassName('table')[0]
 
-function fillTable(){
+function cleanTable(){
+    while (table.firstChild){
+        table.removeChild(table.firstChild)
+    }
+}
+
+function fillTable(content){
+    //clean table
+    cleanTable()
+
+    //fill headers
+    const new_row_header = document.createElement('tr')
+    const new_video_header = document.createElement('th')  
+    new_video_header.innerHTML = "Video"
+    new_row_header.appendChild(new_video_header)
+    const new_ques_header = document.createElement('th')  
+    new_ques_header.innerHTML = "Question"
+    new_row_header.appendChild(new_ques_header)
+    const new_ans_header = document.createElement('th')  
+    new_ans_header.innerHTML = "Answer"
+    new_row_header.appendChild(new_ans_header)
+    table.appendChild(new_row_header)
+
     for (let i = 0; i < content.length; i++){
         let hasRow= false
         for (let j = 0; j < content[i].answers.length; j++){ 
             const new_row = document.createElement('tr')
-            const new_question = document.createElement('td')
-            new_row.appendChild(new_question)
             const new_video = document.createElement('td')   
             new_row.appendChild(new_video)
+            const new_question = document.createElement('td')
+            new_row.appendChild(new_question)
             if (!hasRow){
-                new_question.innerHTML = content[i].question
+                new_question.innerHTML = content[i].question_text
                 new_video.innerHTML = content[i].video
             } 
             hasRow = true
@@ -89,4 +112,27 @@ function fillTable(){
         }
         
     }
+
+    const last_row = document.createElement('tr')
+
+    const last_row_video = document.createElement('td')
+    const add_video_btn = document.createElement('button')
+    add_video_btn.innerHTML = "+"
+    last_row_video.appendChild(add_video_btn)
+    last_row.appendChild(last_row_video)
+
+    const last_row_ques = document.createElement('td')
+    const add_ques_btn = document.createElement('button')
+    add_ques_btn.innerHTML = "+"
+    last_row_ques.appendChild(add_ques_btn)
+    last_row.appendChild(last_row_ques)
+
+    const last_row_ans = document.createElement('td')
+    const add_ans_btn = document.createElement('button')
+    add_ans_btn.innerHTML = "+"
+    last_row_ans.appendChild(add_ans_btn)
+    last_row.appendChild(last_row_ans)
+
+    table.appendChild(last_row)
+
 }

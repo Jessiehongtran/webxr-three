082c896fd6d5a4f6b3297b3583f0886f4d858433
Hong Tran <jessietran@fb.com>
2021-08-25 10:03:39 -0700

    cors issue is gone but a new problem arrives

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const questionRoute = require('./api/question/question.route');
 const answerRoute = require('./api/answer/answer.route');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.use(express.json())
 app.use(cors())
 
